01e886def06ea6f3f85423f84f9c8ab7612bdb85
sahil-singh1107 <sahilpython@gmail.com>
2025-01-16 06:41:38 +0530

    improves form ui

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -29,44 +29,54 @@ const Page = () => {
     if (!values.email || !values.password) return;
     try {
       const result = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/signin`, {
-        email : values.email,
-        password : values.password
+        email: values.email,
+        password: values.password
       })
       reset();
       localStorage.setItem("token", result.data.token);
       router.push("/");
     } catch (error) {
-      console.log(error);    
+      console.log(error);
     }
   }
 
   return (
-    <div className='bg-[#010100] min-h-screen h-dvh w-dvw flex'>
-      <div className='w-[60%]'>
-        <SideBar />
-      </div>
-      <div className='w-[40%] flex flex-col items-center justify-center'>
+    <div className='bg-[#010100] min-h-screen h-dvh w-dvw flex justify-center'>
+      <div className='w-[50%] flex flex-col items-center justify-center'>
         <div className='flex flex-col items-center space-y-2'>
           <span className='text-white text-xl'>Log in to your Account</span>
         </div>
         <form className='flex flex-col space-y-6 mt-16 w-[80%]' onSubmit={form.handleSubmit(onSubmit)}>
           <div className='flex flex-col space-y-1 text-[#808081] hover:text-white duration-200 hover:-translate-y-3'>
             <label htmlFor='email' className='text-xs'>Email</label>
-            <div className='flex justify-end items-center relative'>
-              <input {...register('email')} id='email' type='email' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='johndoe@gmail.com' />
-              <MdOutlineEmail className='absolute mr-2 w-10' />
+            <div className='relative'>
+              <div className={`absolute bg-red-500 inset-0 rounded-xl blur opacity-0 ${errors.email && ' opacity-50'}`}></div>
+
+              <div className='flex justify-end items-center'>
+                <input {...register('email')} id='email' type='email' className='w-full relative bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='johndoe@gmail.com' />
+                <MdOutlineEmail className='absolute mr-2 w-10' />
+              </div>
+
             </div>
+
             {errors.email && <p className='text-xs'>{errors.email.message}</p>}
           </div>
           <div className='flex flex-col space-y-1 text-[#808081] hover:text-white duration-200 hover:-translate-y-3'>
             <label htmlFor='password' className='text-xs'>Password</label>
-            <div className='flex justify-end items-center relative'>
-              <input {...register('password')} id='password' type='password' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' />
-              <FaRegEye className='absolute mr-2 w-10 hover:cursor-pointer' />
+            <div className='relative'>
+              <div className={`absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-red-600 blur ${errors.password ? 'opacity-50' : 'opacity-0'}`}></div>
+              <div className='flex justify-end items-center relative'>
+                <input {...register('password')} id='password' type='password' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' />
+                <FaRegEye className='absolute mr-2 w-10 hover:cursor-pointer' />
+              </div>
             </div>
+
             {errors.password && <p className='text-xs'>{errors.password.message}</p>}
           </div>
-          <button className='bg-white text-black w-full h-10 rounded-xl font-semibold' >Sign Up</button>
+          <div className='relative group'>
+            <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-purple-400 rounded-xl blur-md opacity-75 transition duration-200 group-hover:opacity-100'></div>
+            <button className='bg-white text-black w-full h-10 rounded-xl font-semibold relative' >Sign In</button>
+          </div>
         </form>
         <span className='text-[#555555] text-center mt-2 flex'>Dont have an account? <p className='text-white ml-2 hover:cursor-pointer' onClick={() => {
           router.push("/signup")
--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React from 'react'
-import SideBar from '../components/SideBar'
 import { FaRegUserCircle } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaRegEye } from "react-icons/fa";
@@ -10,8 +9,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { UserSchema } from "@repo/common/config"
 import { z } from "zod"
 import axios from 'axios';
-import {useRouter} from 'next/navigation'
-
+import { useRouter } from 'next/navigation'
 const Page = () => {
     const form = useForm<z.infer<typeof UserSchema>>({
         resolver: zodResolver(UserSchema),
@@ -31,30 +29,27 @@ const Page = () => {
         if (!values.email || !values.firstName || !values.lastName || !values.password || !values.username) return;
         try {
             await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/signup`, {
-                firstName : values.firstName,
-                lastName : values.lastName,
-                username : values.username,
-                email : values.email,
-                password : values.password
+                firstName: values.firstName,
+                lastName: values.lastName,
+                username: values.username,
+                email: values.email,
+                password: values.password
             })
             reset();
             router.push("/signin");
-        } catch (error : any) {
-            if (error.response.data.message[0]==='U') {
-                setError("username", {message : error.response.data.message})
-            }   
+        } catch (error: any) {
+            if (error.response.data.message[0] === 'U') {
+                setError("username", { message: error.response.data.message })
+            }
             else {
-                setError("email", {message : error.response.data.message})
+                setError("email", { message: error.response.data.message })
             }
         }
     }
 
     return (
-        <div className='bg-[#010100] min-h-screen h-dvh w-dvw flex'>
-            <div className='w-[60%]'>
-                <SideBar />
-            </div>
-            <div className='w-[40%] flex flex-col items-center justify-center'>
+        <div className='bg-[#010100] min-h-screen h-dvh w-dvw flex justify-center overflow-hidden'>
+            <div className='w-[50%] flex flex-col items-center justify-center'>
                 <div className='flex flex-col items-center space-y-2'>
                     <span className='text-white text-xl'>Sign Up Account</span>
                     <span className='text-[#555555]'>Enter your personal details to create your account</span>
@@ -63,46 +58,71 @@ const Page = () => {
                     <div className='flex space-x-3 items-center'>
                         <div className='flex flex-col space-y-1 text-[#808081] hover:text-white duration-200 hover:-translate-y-3 relative '>
                             <label htmlFor='firstname' className=' text-xs'>First Name</label>
-                            <div className='flex justify-end items-center relative'>
-                                <input {...register('firstName')} id='firstname' className='bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='John' />
-                                <MdDriveFileRenameOutline className='absolute mr-2 w-10' />
+                            <div className='relative'>
+                                <div className={`absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-red-600 blur ${errors.firstName ? 'opacity-50' : 'opacity-0'}`}></div>
+                                <div className='flex justify-end items-center relative'>
+                                    <input {...register('firstName')} id='firstname' className='bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='John' />
+                                    <MdDriveFileRenameOutline className='absolute mr-2 w-10' />
+                                </div>
                             </div>
+
                             {errors.firstName && <p className='text-xs'>{errors.firstName.message}</p>}
                         </div>
                         <div className='flex flex-col space-y-1 text-[#808081] hover:text-white duration-200 hover:-translate-y-3'>
                             <label htmlFor='lastname' className='text-xs'>Last Name</label>
-                            <div className='flex justify-end items-center relative'>
-                                <input {...register('lastName')} id='lastname' className='bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='Doe' />
-                                <MdDriveFileRenameOutline className='absolute mr-2 w-10' />
+                            <div className='relative'>
+                                <div className={`absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-red-600 blur ${errors.lastName ? 'opacity-50' : 'opacity-0'}`}></div>
+                                <div className='flex justify-end items-center relative'>
+                                    <input {...register('lastName')} id='lastname' className='bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='Doe' />
+                                    <MdDriveFileRenameOutline className='absolute mr-2 w-10' />
+                                </div>
                             </div>
+
                             {errors.lastName && <p className='text-xs'>{errors.lastName.message}</p>}
                         </div>
                     </div>
                     <div className='flex flex-col space-y-1 text-[#808081] hover:text-white duration-200 hover:-translate-y-3'>
                         <label htmlFor='username' className='text-xs'>Username</label>
-                        <div className='flex justify-end items-center relative'>
-                            <input {...register('username')} id='username' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='john_doe' />
-                            <FaRegUserCircle className='absolute mr-2 w-10' />
+                        <div className='relative'>
+                            <div className={`absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-red-600 blur ${errors.username ? 'opacity-50' : 'opacity-0'}`}></div>
+                            <div className='flex justify-end items-center relative'>
+                                <input {...register('username')} id='username' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='john_doe' />
+                                <FaRegUserCircle className='absolute mr-2 w-10' />
+                            </div>
                         </div>
+
                         {errors.username && <p className='text-xs'>{errors.username.message}</p>}
                     </div>
                     <div className='flex flex-col space-y-1 text-[#808081] hover:text-white duration-200 hover:-translate-y-3'>
                         <label htmlFor='email' className='text-xs'>Email</label>
-                        <div className='flex justify-end items-center relative'>
-                            <input {...register('email')} id='email' type='email' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='johndoe@gmail.com' />
-                            <MdOutlineEmail className='absolute mr-2 w-10' />
+                        <div className='relative'>
+                            <div className={`absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-red-600 blur ${errors.email ? 'opacity-50' : 'opacity-0'}`}></div>
+                            <div className='flex justify-end items-center relative'>
+                                <input {...register('email')} id='email' type='email' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' placeholder='johndoe@gmail.com' />
+                                <MdOutlineEmail className='absolute mr-2 w-10' />
+                            </div>
                         </div>
+
+
                         {errors.email && <p className='text-xs'>{errors.email.message}</p>}
                     </div>
                     <div className='flex flex-col space-y-1 text-[#808081] hover:text-white duration-200 hover:-translate-y-3'>
                         <label htmlFor='password' className='text-xs'>Password</label>
+                        <div className='relative'>
+                        <div className={`absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-red-600 blur ${errors.password ? 'opacity-50' : 'opacity-0'}`}></div>
                         <div className='flex justify-end items-center relative'>
                             <input {...register('password')} id='password' type='password' className='w-full bg-[#1b1a1b] h-10 rounded-xl p-6 text-white' />
                             <FaRegEye className='absolute mr-2 w-10 hover:cursor-pointer' />
                         </div>
+                        </div>
+                        
                         {errors.password && <p className='text-xs'>{errors.password.message}</p>}
                     </div>
-                    <button className='bg-white text-black w-full h-10 rounded-xl font-semibold' >Sign Up</button>
+                    <div className='relative group'>
+                        <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-400 to-purple-400 rounded-xl blur-md opacity-75 transition duration-200 group-hover:opacity-100'></div>
+                        <button className='bg-white text-black w-full h-10 rounded-xl font-semibold relative' >Sign Up</button>
+                    </div>
+
                 </form>
                 <span className='text-[#555555] text-center mt-2 flex'>Already have an account? <p className='text-white ml-2 hover:cursor-pointer' onClick={() => {
                     router.push("/signin")
